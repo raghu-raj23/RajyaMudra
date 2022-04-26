48888ddc29d864fa0bdad2207ad56901c692b400
raghu-raj23 <raghuraj.muni@gmail.com>
2022-04-26 16:48:55 +0530

    minor deployment debug

--- rajyamudra/next.config.js
+++ rajyamudra/next.config.js
@@ -2,13 +2,13 @@ module.exports = {
   future: {
     webpack5: false,
   },
-  // webpack: (config, { isServer }) => {
-  //   // Fixes npm packages that depend on `fs` module
-  //   if (!isServer) {
-  //     config.node = {
-  //       fs: 'empty'
-  //     }
-  //   }
-  //   return config
-  // }
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+    return config
+  }
 };
